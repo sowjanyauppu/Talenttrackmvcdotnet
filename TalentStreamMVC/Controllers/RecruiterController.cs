5f40228a7bea3cce66ad4b93814451556f982886
sowjanyauppu <ugsowji@gmail.com>
2024-02-07 19:15:29 +0530

    Add project files.

---
+++ TalentStreamMVC/Controllers/RecruiterController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using TalentStreamMVC.Models;
+
+public class RecruiterController : Controller
+{
+    private readonly string _connectionString = "Server=localhost;Database=talentstreamdotnet;User=root;Password=root;";
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(Recruiter recruiter)
+    {
+        if (ModelState.IsValid)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = "INSERT INTO Recruiters (CompanyName, Email, MobileNumber, Password) " +
+                            "VALUES (@CompanyName, @Email, @MobileNumber, @Password)";
+
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CompanyName", recruiter.CompanyName);
+                    command.Parameters.AddWithValue("@Email", recruiter.Email);
+                    command.Parameters.AddWithValue("@MobileNumber", recruiter.MobileNumber);
+                    command.Parameters.AddWithValue("@Password", recruiter.Password);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToAction("RegistrationSucess");
+        }
+
+        return View(recruiter);
+    }
+
+    public IActionResult RegistrationSucess()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(Login loginModel)
+    {
+      
+        bool isAuthenticated = AuthenticateUser(loginModel.Email, loginModel.Password);
+
+        if (isAuthenticated)
+        {
+             return RedirectToAction("Dashboard");
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Invalid email or password");
+            return View("Login", loginModel);
+        }
+    }
+
+    private bool AuthenticateUser(string email, string password)
+    {
+       
+        using (var connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var query = "SELECT COUNT(*) FROM Recruiters WHERE Email = @Email AND Password = @Password";
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Password", password);
+
+                int count = Convert.ToInt32(command.ExecuteScalar());
+                return count > 0;
+            }
+        }
+    }
+
+    public IActionResult Dashboard()
+    {
+         return View();
+    }
+}
---
+++ TalentStreamMVC/Models/Recruiter.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TalentStreamMVC.Models
+{
+    public class Recruiter
+    {
+        public int recruiterId { get; set; }
+        [Required(ErrorMessage = "Company Name is required")]
+        public string? CompanyName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Mobile Number is required")]
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Invalid Mobile Number")]
+        public string? MobileNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
